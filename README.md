f0a97a2b4257ce15a551749649a9a5fdaddcedac
Gara Naveen <GaraNaveen@JohnDeere.com>
2018-08-20 11:52:19 -0500

    Extracted platform detection to a function in applybashrcchanges.sh

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # linuxcommands
+
+
+applybashrcchanges.sh
+my_aliases - custom aliases to shortcut a lot of commands.
+my_bashrc -  custom functions defined for convenience.
--- applybashrcchanges.sh
+++ applybashrcchanges.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    *)          machine=Linux #Default to Linux
-esac
-echo "This is \"${machine}\" os"
+function setbashrcfile()
+{
+   unameOut="$(uname -s)"
+   case "${unameOut}" in
+       Linux*)     machine=Linux;;
+       Darwin*)    machine=Mac;;
+       *)          machine=Linux #Default to Linux
+   esac
+   echo "This is \"${machine}\" os"
 
+
+   if [ "${machine}" == "Mac" ]
+   then
+      BASHRCFILE=${HOME}/.bash_profile
+   fi
+}
 
 #Default in Linux
 BASHRCFILE=${HOME}/.bashrc
 
-if [ "${machine}" == "Mac" ]
-then
-   BASHRCFILE=${HOME}/.bash_profile
-fi
+setbashrc
 
 echo "Updating ${BASHRCFILE} file to contain custom bashrc and aliases"
 
 cat  bashrcchanges.txt >> ${BASHRCFILE}
+
