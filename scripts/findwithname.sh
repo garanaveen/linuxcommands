a2734acb78fbc8bc2cc474c7536121680ef61de9
garanaveen <garanaveen@gmail.com>
2018-10-10 15:42:39 -0500

    freshinstall.txt - commands to execute on freshinstall

--- scripts/findwithname.sh
+++ scripts/findwithname.sh
@@ -1,5 +1,5 @@
 #EXCLUDE_DIR1/2 #Customize this variable to what you want to exclude.
-EXCLUDE_DIR1=InvalidDir1
+EXCLUDE_DIR1=.jdx
 EXCLUDE_DIR2=InvalidDir2
 find . -type d -name $EXCLUDE_DIR1 -prune  -o -type d -name $EXCLUDE_DIR2 -prune -o -name .hg -prune -o -name .git -prune -o -name "*$1*"  -print
 
