becb50500aae978d5db6bcbf90807bdbbc280847
garanaveen <garanaveen@gmail.com>
2018-10-29 15:04:22 -0500

    Updated grepquicknotes.sh and bunch of other aliases.

--- scripts/grepquicknotes.sh
+++ scripts/grepquicknotes.sh
@@ -1,2 +1,27 @@
-grep $1 ${HOME}/linuxcommands/ -r -C3 --no-filename
-grep $1 ${HOME}/myreference/ -r -C3 --no-filename
+#!/bin/bash
+
+GREP_COMMAND="grep"
+
+#-------------------------------------
+if [ -x "$(command -v ack-grep)" ]; then
+   GREP_COMMAND="ack-grep"
+fi
+
+
+DONT_PRINT_FILENAME="-h"
+CONTEXT_OF_TWO_LINES="-C2"
+
+FOLLOW_LINKS_TOO="-L"
+
+find ${FOLLOW_LINKS_TOO} ${HOME} -maxdepth 3 -name usefulcommands.txt -print |xargs grep ${DONT_PRINT_FILENAME} $1 ${CONTEXT_OF_TWO_LINES}
+
+#-------------------------------------
+
+echo "Press any key........."
+read
+
+${GREP_COMMAND} $1 ${HOME}/linuxcommands/ -r -C3 -i --no-filename
+${GREP_COMMAND} $1 ${HOME}/myreference/ -r -C3 -i --no-filename
+${GREP_COMMAND} $1 ${HOME}/dl/ -r -C3 -i --no-filename
+
+
