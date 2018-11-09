33287ac8d6520bf09c667b22e764b5b9738b24b4
Naveen Gara <garanaveen@johndeere.com>
2018-11-09 09:57:43 -0600

    fromquora.sh strip of space characters at the beginning of the line.

--- scripts/fromquora.sh
+++ scripts/fromquora.sh
@@ -3,5 +3,5 @@
 TMP_FILE=${HOME}/tmp/q.html
 wget $1 -O ${TMP_FILE}
 tidy -mi -html -wrap 0 -q ${TMP_FILE}
-grep -f ~/linuxcommands/scripts/quorasearchpattern.txt ${TMP_FILE} | sed -e 's/<[^>]*>//g' | sed -e 's/&#\d*//g' > ~/tmp/a
-cat  ~/tmp/a  | sed 's/^ *//g'
+grep -f ~/linuxcommands/scripts/quorasearchpattern.txt ${TMP_FILE} | sed -e 's/<[^>]*>//g' | sed -e 's/&#\d*//g' | sed -e 's/^ *//' > ~/tmp/a
+
