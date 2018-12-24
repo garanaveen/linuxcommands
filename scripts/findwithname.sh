45fe86575f692742d9019c8e1757da55db42e875
garanaveen <garanaveen@gmail.com>
2018-12-24 10:11:23 -0600

    ignore directories in ackrc

--- scripts/findwithname.sh
+++ scripts/findwithname.sh
@@ -1,6 +1,6 @@
 #EXCLUDE_DIR1/2 #Customize this variable to what you want to exclude.
 EXCLUDE_DIR1=.jdx
-EXCLUDE_DIR2=InvalidDir2
+EXCLUDE_DIR2=jdx
 find . -type d -name $EXCLUDE_DIR1 -prune  -o -type d -name $EXCLUDE_DIR2 -prune -o -name .hg -prune -o -name .git -prune -o -name "*$1*"  -print
 
 
