54f3631c538bc056970e6286f981307eb4c9e5b2
garanaveen <garanaveen@gmail.com>
2019-01-31 17:19:00 -0600

    vimfindfirstoccurance.sh added vimf alias

--- scripts/vimfindfirstoccurance.sh
+++ scripts/vimfindfirstoccurance.sh
@@ -2,6 +2,7 @@
 EXCLUDE_DIR1=.jdx
 EXCLUDE_DIR2=InvalidDir2
 FILEPATH=`find . -type d -name $EXCLUDE_DIR1 -prune  -o -type d -name $EXCLUDE_DIR2 -prune -o -name .hg -prune -o -name .git -prune -o -name "*$1*"  -print -quit`
+echo "FILEPATH : ${FILEPATH}"
 vim ${FILEPATH}
 
 
