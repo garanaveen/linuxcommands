331461029d7b27aea200e3dcbfaca42886e8e307
garanaveen <garanaveen@gmail.com>
2019-04-05 13:41:32 -0500

    Added CMakeLists.txt to cscope generation script

--- scripts/generatecscopefile.sh
+++ scripts/generatecscopefile.sh
@@ -2,6 +2,7 @@ find .  \
 -name jdx -type d -prune \
 -o -name \*.cpp -exec echo \"{}\" \; \
 -o -name \*.h -exec echo \"{}\" \; \
+-o -name CMakeLists.txt -exec echo \"{}\" \; \
 > cscope.files
 
 
