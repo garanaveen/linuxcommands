27a3d612f9e9d4cffc73de0b288aabca206e95eb
garanaveen <garanaveen@gmail.com>
2019-05-23 13:04:50 -0500

    gitrepeat command fix and grepquickcontacts now searches ~/jdw too

--- scripts/gitrepeatforallrepos.sh
+++ scripts/gitrepeatforallrepos.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #This script is supposed to do "git pull" on all the repos in the current directory.
-#find . -maxdepth 1  -mindepth 1 -type d -exec sh -c 'git --git-dir="$0"/.git pull' {} \;
+#find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'git --git-dir="$0" pull' {} \;
 
 #This command generates the gitpull for all the repos in the current directory. Helpful in deleting all the repos and recloning them.
-#find . -maxdepth 1  -mindepth 1 -type d -exec sh -c 'git --git-dir="$0"/.git remote -v' {} \; 2>1 |grep push | sed 's/origin/git clone/' |sed 's/ (push)//'
+#find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'git --git-dir="$0" remote -v' {} \; 2>1 |grep push | sed 's/origin/git clone/' |sed 's/ (push)//'
+
+
--- scripts/grepquicknotes.sh
+++ scripts/grepquicknotes.sh
@@ -17,12 +17,17 @@ find ${FOLLOW_LINKS_TOO} ${HOME} -maxdepth 3 -name usefulcommands.txt -print |xa
 
 #-------------------------------------
 
-echo "Press enter key........."
-read
-
 ${GREP_COMMAND} $1 ${HOME}/linuxcommands/ -r -C3 -i --no-filename
 ${GREP_COMMAND} $1 ${HOME}/myreference/ -r -C3 -i --no-filename
+echo "----Results in linuxcommands and myreferences"
 
 echo "Press enter key........."
+read
 ${GREP_COMMAND} $1 ${HOME}/ds/ -r -C3 -i --no-filename
 ${GREP_COMMAND} $1 ${HOME}/dl/ -r -C3 -i --no-filename
+echo "----Results from ds and dl"
+
+echo "Press enter key........."
+read
+${GREP_COMMAND} $1 ${HOME}/jdw/ -r -C3 -i --no-filename
+echo "----Results from jdw"
