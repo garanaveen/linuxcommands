b6f711ae9e14661f51bf0b11d3397a36c97d24b4
garanaveen <garanaveen@gmail.com>
2019-06-27 14:35:42 -0500

    cdfindfirstoccurance.sh working
    findwithname.sh beautify
    
    cdfindfirstoccurance.sh and vimfindfirstoccurance.sh beautify

--- scripts/cdfindfirstoccurance.sh
+++ scripts/cdfindfirstoccurance.sh
@@ -1,9 +1,16 @@
 #Similar file : vimfindfirstoccurance.sh
 
 #EXCLUDE_DIR1/2 #Customize this variable to what you want to exclude.
-EXCLUDE_DIR1=.jdx
-EXCLUDE_DIR2=InvalidDir2
-export FILEPATH=`find . -type d -name $EXCLUDE_DIR1 -prune  -o -type d -name $EXCLUDE_DIR2 -prune -o -name .hg -prune -o -name .git -prune -o -name "*$1*"  -print -quit`
+EXCLUDE_DIR1=jdx
+EXCLUDE_DIR2=Dir2-ReplaceThisAsPerYourNeeds
+FILEPATH=`find . \
+-type d -name $EXCLUDE_DIR1 -prune \
+-o -type d -name $EXCLUDE_DIR2 -prune \
+-o -name .hg -prune \
+-o -name .git -prune \
+-o -name "*$1*" \
+-print -quit`
 echo "FILEPATH : ${FILEPATH}"
-#cd ${FILEPATH} #This is not working. Need to figure out a way to make the cd work in the terminal which forked this script.
+cd ${FILEPATH} #This will work only if you source this script (like 'source cdfindfirstoccurance.sh' instead of './cdfindfirstoccurance.sh')
+
 
--- scripts/vimfindfirstoccurance.sh
+++ scripts/vimfindfirstoccurance.sh
@@ -1,9 +1,17 @@
 #Similar file : cdfindfirstoccurance.sh
 
-#EXCLUDE_DIR1/2 #Customize this variable to what you want to exclude.
-EXCLUDE_DIR1=.jdx
-EXCLUDE_DIR2=InvalidDir2
-FILEPATH=`find . -type d -name $EXCLUDE_DIR1 -prune  -o -type d -name $EXCLUDE_DIR2 -prune -o -name .hg -prune -o -name .git -prune -o -name "*$1*"  -print -quit`
+EXCLUDE_DIR1=jdx
+EXCLUDE_DIR2=Dir2-ReplaceThisAsPerYourNeeds
+#Add more exclude paths as per your need.
+
+FILEPATH=`find . \
+-type d -name $EXCLUDE_DIR1 -prune \
+-o -type d -name $EXCLUDE_DIR2 -prune \
+-o -name .hg -prune \
+-o -name .git -prune \
+-o -name "*$1*" \
+-print -quit`
+
 echo "FILEPATH : ${FILEPATH}"
 vim ${FILEPATH}
 
