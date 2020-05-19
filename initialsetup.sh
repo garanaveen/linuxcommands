8871b7cbfacefb1042077a0d31f166db17a1b913
garanaveen <garanaveen@gmail.com>
2020-05-19 11:11:59 -0500

    ackrc

--- initialsetup.sh
+++ initialsetup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+THIS_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+echo "THIS_DIR : $THIS_DIR"
+
+export LINUXCOMMANDS_ROOT=$PWD/
+
 #-h is test if file exists and is a link.
 if [ ! -h "$HOME/linuxcommands" ]; then
     ln -sv $PWD $HOME/linuxcommands
 fi
 
-export LINUCCOMMANDS_ROOT=$PWD/
 
 #Git config
 git config --local user.email "garanaveen@gmail.com"
 git config --local user.name "garanaveen"
 
+
+cat ackrc | tail -n +3 >> $HOME/.ackrc
 
