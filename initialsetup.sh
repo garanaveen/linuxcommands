2f4c72da4a9d5123d83805115ab1f01035ea36da
garanaveen <garanaveen@gmail.com>
2020-07-05 14:32:00 -0500

    linuxcommands/vimrc to .vimrc in initialsetup

--- initialsetup.sh
+++ initialsetup.sh
@@ -4,11 +4,16 @@ THIS_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 echo "THIS_DIR : $THIS_DIR"
 
 export LINUXCOMMANDS_ROOT=$PWD/
+echo "LINUXCOMMANDS_ROOT : $LINUXCOMMANDS_ROOT"
+
 
 #-h is test if file exists and is a link.
 if [ ! -h "$HOME/linuxcommands" ]; then
     ln -sv $PWD $HOME/linuxcommands
 fi
+
+cat $HOME/linuxcommands/vimrc >> $HOME/.vimrc 
+
 
 
 #Git config
