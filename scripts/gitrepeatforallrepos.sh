a565801b8c58ee17bcb977e93ac550e038521e76
garanaveen <garanaveen@gmail.com>
2020-11-09 10:11:46 -0600

    Fixed gitrepeatforallrepos.sh

--- scripts/gitrepeatforallrepos.sh
+++ scripts/gitrepeatforallrepos.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-#This script is supposed to do "git pull" on all the repos in the current directory.
-#find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'git --git-dir="$0" pull' {} \;
-#find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'cd $0 ; cd ../ ;  git pull ; cd ..' {} \;
+#These are scripts that are supposed to execute git commands on all the repos in the current directory (not recursive).
+
+#Check if there are any incoming messages from remote,
+#find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'cd $0 ; cd ../ ; pwd ; git log master..origin/master; cd ..' {} \;
+
+#git pull
+#find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'cd $0 ; cd ../ ; pwd ; git pull ; cd ..' {} \;
 
 #This command generates the gitpull for all the repos in the current directory. Helpful in deleting all the repos and recloning them.
 #find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'git --git-dir="$0" remote -v' {} \; 2>1 |grep push | sed 's/origin/git clone/' |sed 's/ (push)//'
