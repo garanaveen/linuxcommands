53bf80e1fddec1b34e571b6a7baa9685273aa825
garanaveen <garanaveen@gmail.com>
2020-11-10 12:05:07 -0600

    PRINT_FILE_NAME_OPTION made flexible in grepquicknotes.sh

--- scripts/grepquicknotes.sh
+++ scripts/grepquicknotes.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
 
-GREP_COMMAND="grep"
-
-#-------------------------------------
-if [ -x "$(command -v ack)" ]; then
-   GREP_COMMAND="ack"
-fi
-
-DONT_PRINT_FILENAME="-h"
-CONTEXT_OF_TWO_LINES="-C2"
-
+SEARCH_PATTERN="$1"
+GREP_COMMAND="ack"
 FOLLOW_LINKS_TOO="-L"
+#PRINT_FILE_NAME_OPTION="--no-filename"
+PRINT_FILE_NAME_OPTION=""
 
-find ${FOLLOW_LINKS_TOO} ${HOME} -maxdepth 3 -name usefulcommands.txt -print |xargs grep ${DONT_PRINT_FILENAME} "$1" ${CONTEXT_OF_TWO_LINES}
+find ${FOLLOW_LINKS_TOO} ${HOME} -maxdepth 3 -name usefulcommands.txt -print |xargs grep ${PRINT_FILE_NAME_OPTION} ${SEARCH_PATTERN} --context=2
+echo "----Results in all usefulcommands.txt"
+read
+echo "Press enter key to continue searching........."
 
 #-------------------------------------
 
-${GREP_COMMAND} $1 ${HOME}/linuxcommands/ -r -C3 -i --no-filename
-${GREP_COMMAND} $1 ${HOME}/myreference/ -r -C3 -i --no-filename
-${GREP_COMMAND} $1 ${HOME}/personal/ -r -C3 -i --no-filename
+${GREP_COMMAND} $1 ${HOME}/linuxcommands/ -r --context=3 -i ${PRINT_FILE_NAME_OPTION}
+${GREP_COMMAND} $1 ${HOME}/myreference/ -r --context=3 -i ${PRINT_FILE_NAME_OPTION}
+${GREP_COMMAND} $1 ${HOME}/personal/ -r --context=3 -i ${PRINT_FILE_NAME_OPTION}
 echo "----Results in linuxcommands and myreferences"
 
 echo "Press enter key to continue searching........."
 read
-${GREP_COMMAND} $1 ${HOME}/ds/ -r -C3 -i --no-filename
-${GREP_COMMAND} $1 ${HOME}/dl/ -r -C3 -i --no-filename
+${GREP_COMMAND} $1 ${HOME}/ds/ -r --context=3 -i ${PRINT_FILE_NAME_OPTION}
+${GREP_COMMAND} $1 ${HOME}/dl/ -r --context=3 -i ${PRINT_FILE_NAME_OPTION}
 echo "----Results from ds and dl"
 
 echo "Press enter key to continue searching........."
 read
-${GREP_COMMAND} $1 ${HOME}/github/garanaveen/ -r -C3 -i --no-filename
+${GREP_COMMAND} $1 ${HOME}/github/garanaveen/ -r --context=3 -i ${PRINT_FILE_NAME_OPTION}
 echo "----Results from github/garanaveen"
 
 echo "Press enter key to continue searching........."
 read
-${GREP_COMMAND} $1 ${HOME}/jdw/ -r -C3 -i
+${GREP_COMMAND} $1 ${HOME}/jdw/ -r --context=3 -i
 echo "----Results from jdw"
 
