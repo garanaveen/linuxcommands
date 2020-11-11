d7daebc38b3ff5c6190128d32bc6afdfac55c6a4
garanaveen <garanaveen@gmail.com>
2020-11-11 14:23:32 -0600

    spell correction

--- scripts/gitrepeatforallrepos.sh
+++ scripts/gitrepeatforallrepos.sh
@@ -8,7 +8,7 @@
 #git pull
 #find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'cd $0 ; cd ../ ; pwd ; git pull ; cd ..' {} \;
 
-#This command generates the gitpull for all the repos in the current directory. Helpful in deleting all the repos and recloning them.
+#This command generates the list of git clone commands for all the repos in the current directory. Helpful in deleting all the repos and recloning them.
 #find . -maxdepth 2  -mindepth 1 -name .git -type d -exec sh -c 'git --git-dir="$0" remote -v' {} \; 2>1 |grep push | sed 's/origin/git clone/' |sed 's/ (push)//'
 
 
