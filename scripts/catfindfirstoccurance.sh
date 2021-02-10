69efacbefe1a94c800a0c9b5a928b76d5678d225
garanaveen <garanaveen@gmail.com>
2021-02-09 21:07:40 -0600

    fullpath.sh and catfindfirstoccurance.sh added

---
+++ scripts/catfindfirstoccurance.sh
@@ -0,0 +1,18 @@
+#Similar file : cdfindfirstoccurance.sh
+
+EXCLUDE_DIR1=jdx
+EXCLUDE_DIR2=Dir2-ReplaceThisAsPerYourNeeds
+#Add more exclude paths as per your need.
+
+FILEPATH=`find . \
+-type d -name $EXCLUDE_DIR1 -prune \
+-o -type d -name $EXCLUDE_DIR2 -prune \
+-o -name .hg -prune \
+-o -name .git -prune \
+-o -name "*$1*" \
+-print -quit`
+
+cat ${FILEPATH}
+
+echo "FILEPATH : ${FILEPATH}"
+
--- scripts/fullpath.sh
+++ scripts/fullpath.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo $PWD/$1
+echo "cp $PWD/$1 ./"
+
