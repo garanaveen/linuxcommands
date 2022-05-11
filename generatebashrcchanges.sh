14be40797f17830bd37d192729951d4a1b7dc04b
garanaveen <garanaveen@gmail.com>
2022-05-10 22:02:41 -0500

    bashrc seperated to _all _linux and _mac

--- generatebashrcchanges.sh
+++ generatebashrcchanges.sh
@@ -18,21 +18,25 @@ then
 	#echo "Mac"
     BASHRCFILE=${HOME}/.bash_profile
     PLATFORM_SPECIFIC_ALIASES=mac_aliases
+    PLATFORM_SPECIFIC_BASHRC=bashrc_mac
 else
 	#echo "Non Mac"
     BASHRCFILE=${HOME}/.bashrc
     PLATFORM_SPECIFIC_ALIASES=linux_aliases
+    PLATFORM_SPECIFIC_BASHRC=bashrc_linux
 fi
 
 #echo "Updating ${BASHRCFILE} file to contain custom bashrc and aliases"
 
 declare -a FILE_LIST=(
                       ${CURRENT_DIR}/${PLATFORM_SPECIFIC_ALIASES} 
-                      ${CURRENT_DIR}/bashrc 
+                      ${CURRENT_DIR}/${PLATFORM_SPECIFIC_BASHRC} 
+                      ${CURRENT_DIR}/bashrc_all
                       ${CURRENT_DIR}/aliases
                       ${CURRENT_DIR}/git_aliases
                      )
 echo "#./generatebashrcchanges.sh >> $HOME/.bashrc"
+echo "#./generatebashrcchanges.sh >> $HOME/.bash_profile"
 echo "#Start===================="
 for i in "${FILE_LIST[@]}"
 do
