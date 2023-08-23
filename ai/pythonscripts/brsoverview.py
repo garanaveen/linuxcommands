ea1c5dd4e4f5012003f306150a43a126a0548162
Naveen Gara <ngara@roku.com>
2023-08-23 08:19:30 -0500

    optional string for pythonscripts/brsoverview.py

--- ai/pythonscripts/brsoverview.py
+++ ai/pythonscripts/brsoverview.py
@@ -4,6 +4,7 @@ import re
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Print lines with Sub or Function declarations from a BrightScript file")
 parser.add_argument("file_path", help="Path to the BrightScript file")
+parser.add_argument("--optional_string", help="Optional string to match along with Sub/Function", default=None)
 args = parser.parse_args()
 
 # Regular expression pattern for matching "Sub" or "Function" as whole words
@@ -14,5 +15,7 @@ with open(args.file_path, "r") as file:
     for line in file:
         line = line.strip()  # Remove leading/trailing whitespace
         if pattern.match(line) and "end " not in line.lower():
+            print(line)
+        elif args.optional_string and args.optional_string.lower() in line.lower():
             print(line)
 
