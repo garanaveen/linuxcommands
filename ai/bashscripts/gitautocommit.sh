5156aab81f562091943d639af1b07dcf648623bc
garanaveen <garanaveen@gmail.com>
2023-09-20 14:43:27 -0500

    Auto commit - Date: 2023-09-20, Host: D888N83

--- ai/bashscripts/gitautocommit.sh
+++ ai/bashscripts/gitautocommit.sh
@@ -9,6 +9,17 @@ hostname=$(hostname)
 # Create the commit message
 commit_message="Auto commit - Date: $current_date, Host: $hostname"
 
-# Run the git commit command
-git commit -m "$commit_message"
+# Show the changes that would be committed
+git status
+
+# Ask for confirmation
+read -p "Do you want to proceed with the commit? (y/n): " confirm
+
+if [ "$confirm" == "y" ]; then
+    # Run the git commit command
+    git commit -a -m "$commit_message"
+    echo "Commit successful!"
+else
+    echo "Commit aborted."
+fi
 
