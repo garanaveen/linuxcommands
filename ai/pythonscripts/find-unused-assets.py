d44d92a2a89a9d69b8ca5349ccfbe65872e74418
Naveen Gara <ngara@roku.com>
2025-06-10 08:52:54 -0500

    Auto commit - Date: 2025-06-10_08:52, Host: ngara-mousetrap

---
+++ ai/pythonscripts/find-unused-assets.py
@@ -0,0 +1,9 @@
+# @todo :
+
+# Write a python script to find unused assets in a project.
+
+# The script should take a directory path under which the assets are stored. The script should os.walk the directory and find all files with extensions .png, .webp
+
+# The script should then check if these files are used in the project by searching for their references in the codebase. If any assets are not referenced in the codebase, they should be printed as such.
+
+# The repository should be the one where the script is run from.
